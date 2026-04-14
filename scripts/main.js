b9a42264fac02e65951c3f294ca543518407f4e8
WIheee <bywihee@outlook.com>
2026-04-14 23:02:29 +0800

    移除提示

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,9 @@ import { world, system } from "@minecraft/server"
 // 导入所有命令
 import "./command/index.js"
 
-// 提示
-world.afterEvents.playerJoin.subscribe((event) => {
-    event.player.sendMessage("§7[§bGrimAC§7] §b输入 §ehelp §b查看可用命令")
-})
-
 // 心跳包
 let number = 0
 system.runInterval(() => {
     world.sendMessage(`§7[§bGrimAC§7] §bGrimAC §aheartbeat packet §i(${number})`)
-    number = number+1
-}, 6000)
+    number = number + 1
+}, 6000) 
